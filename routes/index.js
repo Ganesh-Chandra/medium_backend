cc45980cf0095dc45a5a5143a79c332cdc73bb92
Ganesh-Chandra <ganeshchandrasaladi@gmail.com>
2020-06-03 17:44:08 +0530

    after execution conformation

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.send("cool");
+  res.status(200).send("cool");
 });
 
 module.exports = router;
